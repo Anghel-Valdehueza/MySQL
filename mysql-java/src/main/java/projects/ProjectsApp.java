762b335787693c2710fbc6047c5a3e3032d10089
Anghel Valdehueza <Angvaldehueza@gmail.com>
2022-12-03 20:35:55 -0600

    Week 11 Update and Delete a Project.

--- mysql-java/src/main/java/projects/ProjectsApp.java
+++ mysql-java/src/main/java/projects/ProjectsApp.java
@@ -24,7 +24,9 @@ public class ProjectsApp {
 	private List<String> operations = List.of(
 			"1) Add a project",
 			"2) List projects",
-			"3) Select a project"
+			"3) Select a project",
+			"4) Update project details",
+			"5) Delete a project"
 
 	);
 	//@formatter:on
@@ -48,7 +50,6 @@ public class ProjectsApp {
 					done = exitMenu();
 					break;
 					
-					
 					case 1:
 					createProject();
 					break;
@@ -61,9 +62,17 @@ public class ProjectsApp {
 						selectProject();
 						break;
 
+					case 4:
+						updateProjectDetails();
+						break;
+	
+					case 5:
+						deleteProject();
+						break;
+
 					default:
 					System.out.println("\n" + selection + " is not a valid selection. Try again.");
-			
+					break;
 				}
 			}
 			catch(Exception e) {
@@ -75,6 +84,49 @@ public class ProjectsApp {
 	}
 
 	
+
+	private void deleteProject() {
+		listProjects();
+		
+		Integer projectId = getIntInput("Enter the ID of the project to delete");
+		
+		projectService.deleteProject(projectId);
+		System.out.println("Project " + projectId + " was deleted successfully.");
+		
+		if(Objects.nonNull(curProject) && curProject.getProjectId().equals(projectId) ) {
+			curProject = null;
+		}
+	
+		
+	}
+
+	private void updateProjectDetails() {
+		if(Objects.isNull(curProject)) {
+			System.out.println("\nPlease select a project.");
+			return;
+		}
+
+		String projectName = getStringInput("Enter the project name [" + curProject.getProjectName() + "]");
+		BigDecimal estimatedHours = getDecimalInput("Enter the estimated hours [" + curProject.getEstimatedHours() + "]");
+		BigDecimal actualHours = getDecimalInput("Enter the actual hours [" + curProject.getActualHours() + "]");
+		Integer difficulty = getIntInput("Enter the project difficulty (1-5) [" + curProject.getDifficulty() + "]");
+		String notes = getStringInput("Enter the project notes [" + curProject.getNotes() + "]");
+
+		Project project = new Project();
+		
+		project.setProjectId(curProject.getProjectId());
+		project.setProjectName(Objects.isNull(projectName) ? curProject.getProjectName() : projectName);
+		
+		project.setEstimatedHours(Objects.isNull(estimatedHours) ? curProject.getEstimatedHours() : estimatedHours);
+	
+		project.setActualHours(Objects.isNull(actualHours) ? curProject.getActualHours() : actualHours);
+		project.setDifficulty(Objects.isNull(difficulty) ? curProject.getDifficulty() : difficulty);
+		project.setNotes(Objects.isNull(notes) ? curProject.getNotes() : notes);
+
+		projectService.modifyProjectDetails(project);
+		curProject = projectService.fetchProjectById(curProject.getProjectId());
+
+	}
 
 	private void selectProject() {
 		listProjects();
--- mysql-java/src/main/java/projects/service/ProjectService.java
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -6,6 +6,7 @@ import projects.dao.ProjectDao;
 import projects.entity.Project;
 //import projects.exception.DbException;
 //import java.nio.file.Files;
+import projects.exception.DbException;
 
 public class ProjectService {
 	private ProjectDao projectDao = new ProjectDao();
@@ -24,6 +25,18 @@ public class ProjectService {
 
 	public Project fetchProjectById(Integer projectId) {
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
+	}
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+		throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+	}
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+		throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
 	}
 
 	
